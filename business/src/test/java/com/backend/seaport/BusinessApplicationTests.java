2fb85448588107b914300efdc4c6b6e379a4e8e6
christian <pearson3135@gmail.com>
2019-09-30 14:55:31 -0500

    backend

---
+++ business/src/test/java/com/backend/seaport/BusinessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.backend.seaport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BusinessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
