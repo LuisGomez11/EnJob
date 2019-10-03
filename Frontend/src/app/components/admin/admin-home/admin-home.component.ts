cf2665508859aea9bb76ed0614a10e50c3984b01
Luis Alberto Gomez Arteaga <luisgomez24g@gmail.com>
2019-10-03 12:43:37 -0500

    Frontend

--- Frontend/src/app/components/admin/admin-home/admin-home.component.ts
+++ Frontend/src/app/components/admin/admin-home/admin-home.component.ts
@@ -15,6 +15,22 @@ export class AdminHomeComponent implements OnInit {
     $('#nav-principal').hide();
     $('#nav-admin').show();
     $('.menu-admin').show();
+    $(window).resize(function(){
+
+      if ($(window).width() <= 991) {  
+      $('.menu-admin').hide();
+      	$('.menu-admin').css("width", "100%");
+      	$('.contenedor').css("margin-left", "0");
+      	$('#header-info').addClass("header-sm");
+      	$('#header-info').css("float", "left");
+      }  else{
+      $('.menu-admin').show();
+		$('.menu-admin').css("width", "280px");
+		$('.contenedor').css("margin-left", "280px");
+		$('#header-info').removeClass("header-sm");
+      }   
+
+});
   }
 
 }
