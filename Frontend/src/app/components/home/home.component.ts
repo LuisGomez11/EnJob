b35105f6149fca7da8c226581500e4afcaaff65a
Luis Alberto Gomez Arteaga <luisgomez24g@gmail.com>
2019-10-11 16:30:43 -0500

    Home

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -12,11 +12,26 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $('.logo-nav').hide();
     $('#nav-principal').show();
     $('#nav-admin').hide();
     $('.menu-admin').hide();
     $('#nav-sadmin').hide();
     $('#nav-supervisor').hide();
+    $('#nav-empo').hide();
+    $(window).scroll(function () {
+      var scrollPos = $(window).scrollTop();
+
+      if(scrollPos>0){
+        $('#nav-principal').removeClass('bg-transparent');
+        $('#nav-principal').addClass('bg-white');
+        $('.logo-nav').show();
+      }else{
+        $('#nav-principal').addClass('bg-transparent');
+        $('#nav-principal').removeClass('bg-white');
+        $('.logo-nav').hide();
+      }
+    });
   }
 
 }
