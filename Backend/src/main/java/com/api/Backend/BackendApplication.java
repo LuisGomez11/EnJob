5ebc075545ca5bd38c054f232d3d2ae6ac4b3410
Luis Alberto Gomez Arteaga <luisgomez24g@gmail.com>
2019-10-17 20:47:24 -0500

    Backend

--- Backend/src/main/java/com/api/Backend/BackendApplication.java
+++ Backend/src/main/java/com/api/Backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.business.backend;
+package com.api.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BusinessBackendApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BusinessBackendApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
