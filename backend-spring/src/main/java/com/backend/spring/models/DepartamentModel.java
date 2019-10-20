671fbd45c1d3f1d1a20cf3aa93a61aaefe0b4b99
christian <pearson3135@gmail.com>
2019-10-20 18:16:43 -0500

    f

--- backend-spring/src/main/java/com/backend/spring/models/DepartamentModel.java
+++ backend-spring/src/main/java/com/backend/spring/models/DepartamentModel.java
@@ -3,6 +3,11 @@
  */
 package com.backend.spring.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.Data;
 
 /**
@@ -11,5 +16,14 @@ import lombok.Data;
  */
 @Data
 public class DepartamentModel {
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid2")
+	private String idDpt;
+	private String nameDpt;
+	private String enterpriseDpt;
 	
+	public DepartamentModel() {
+		
+	}
 }
