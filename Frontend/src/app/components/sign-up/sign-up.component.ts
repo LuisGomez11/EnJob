3ba2212ac8d2045dc9a998f776db9ce76217cef6
Luis Alberto Gomez Arteaga <luisgomez24g@gmail.com>
2019-10-21 19:15:28 -0500

    form

--- Frontend/src/app/components/sign-up/sign-up.component.ts
+++ Frontend/src/app/components/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdmineModel } from '../../models/admine/admine-model';
 import { AdmineServiceService } from '../../services/admine/admine-service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 declare var $ : any;
 
@@ -11,22 +12,12 @@ declare var $ : any;
 })
 export class SignUpComponent implements OnInit {
 
+  public form: FormGroup;
+
   public admine: AdmineModel;
   // private admineService : AdmineServiceService
-  constructor(private admineService: AdmineServiceService) {
-    this.admine = new AdmineModel (
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-  )
+  constructor(private admineService: AdmineServiceService, private formBuilder: FormBuilder) {
+
    }
 
   ngOnInit() {
@@ -36,11 +27,22 @@ export class SignUpComponent implements OnInit {
     $('#nav-sadmin').hide();
     $('#nav-supervisor').hide();
     $('#nav-empo').hide();
-    
+
+    this.form = this.formBuilder.group({
+     name: ['', Validators.required],
+     lastName: ['', Validators.required],
+     numDocument: ['', Validators.required],
+     phone: ['', Validators.required],
+     email: ['', Validators.required],
+     nameCompany: ['', Validators.required],
+     userName: ['', Validators.required],
+     password: ['', Validators.required]
+    });
+
   }
 
   signUp(){
-    this.admineService.signup(form)
+    this.admineService.signup(this.form.value)
       .subscribe(
         res => {
           console.log(res);
