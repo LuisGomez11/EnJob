be8167e09a9cb8d88ce4ee4f6731c183669a2da8
christian <pearson3135@gmail.com>
2019-10-23 12:40:02 -0500

    l

--- backend-node/src/util/password.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const {plantedPassword} = require('../configs/data');
-
-exports.compare = password => {
-    let lo = bcrypt.compareSync(plantedPassword, password)
-    if(lo == false) return false;
-    return lo;
-}
