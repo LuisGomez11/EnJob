3ab37d27b2eac8ff753981ec5cb8e93a071465c1
Luis Alberto Gomez Arteaga <luisgomez24g@gmail.com>
2019-10-24 23:39:14 -0500

    login

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -11,13 +11,9 @@ declare var $ : any;
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-<<<<<<< HEAD
   role = '';
+  
   constructor(private auth: AuthAdmineService, private router: Router) { }
-=======
-
-  constructor(private authService: AuthAdmineService, private router: Router) { }
->>>>>>> 76c09990ccf339afcfbc21f7fd2fc514122b4f32
 
   ngOnInit() {
     $('#nav-principal').hide();
@@ -28,7 +24,6 @@ export class LoginComponent implements OnInit {
     $('#nav-empo').hide();
   }
   onLogin(form): void {
-<<<<<<< HEAD
 
       this.auth.login(form.value).subscribe(res => {
         this.role = this.auth.AdmineUser;
@@ -39,11 +34,6 @@ export class LoginComponent implements OnInit {
         }
         
       });
-    
-=======
-    this.authService.login(form.value).subscribe(res => {
-      this.router.navigateByUrl('/admin');
-    });
->>>>>>> 76c09990ccf339afcfbc21f7fd2fc514122b4f32
+
   }
 }
