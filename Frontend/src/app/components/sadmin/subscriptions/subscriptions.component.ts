8ce158a13986352cd17db0e8c9286509a0f4c4f2
LuisGomez11 <luisgomez24g@gmail.com>
2019-11-17 23:06:11 -0500

    change

--- Frontend/src/app/components/sadmin/subscriptions/subscriptions.component.ts
+++ Frontend/src/app/components/sadmin/subscriptions/subscriptions.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 
 @Component({
-  selector: 'app-subscribed',
-  templateUrl: './subscribed.component.html',
-  styleUrls: ['./subscribed.component.css']
+  selector: 'app-subscriptions',
+  templateUrl: './subscriptions.component.html',
+  styleUrls: ['./subscriptions.component.css']
 })
-export class SubscribedComponent implements OnInit {
+export class SubscriptionsComponent implements OnInit {
 
   constructor(public app : AppComponent) { }
 
