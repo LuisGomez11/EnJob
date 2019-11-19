fe472fbdc9f4aea5115276d905375ba7de61d20c
christian <pearson3135@gmail.com>
2019-11-18 19:43:58 -0500

    k

--- backend-node/src/routes/photoRoute.js
+++ backend-node/src/routes/photoRoute.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const multer = require('../middleware/multer');
   const {uploadPhoto} = require('../controller/photoController');
 
-  app.post('/v1/api/photo/upload/:id', multer.single('image'), uploadPhoto );
+  app.put('/v1/api/photo/upload/:id', multer.single('image'), uploadPhoto );
 
   app.get('*', (req, res) => {
     res.status(500).send({ err: 'servido no encontrado' });
